37430880fac5b38f0248e8a117ff053c1e439d31
YA <YA-androidapp@users.noreply.github.com>
2019-07-17 12:57:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from seleniumrequests import Firefox
 import codecs
 import datetime
 import os
@@ -37,12 +38,13 @@ DATA_FILEPATH = os.path.join(
 LOG_FILEPATH = os.path.join(
     currentdirectory, 'log_'+datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.txt')
 
+PERSONS_PER_PAGE = 20
 WAITING_TIME = 10000
 
 # URI
 baseUris = [
     # https://www.talent-databank.co.jp/guideline/index.html
-    'https://www.talent-databank.co.jp',
+    'https://www.talent-databank.co.jp/search/',
     'https://talemecasting-next.com'  # https://talemecasting-next.com/agreement
 ]
 targetUris = [
@@ -50,11 +52,11 @@ targetUris = [
     'https://talemecasting-next.com/talent?birth_year_min=&birth_year_max=&age_min=22&age_max=24&genre%5B%5D=15&height_min=&height_max=&weight_min=&weight_max=&blood=&constellation=&keyword=&carrier='
 ]
 
-# カテゴリ毎に、取得するページ数
-MAX_PAGE = [
-    20,
-    20
-]
+# # カテゴリ毎に、取得するページ数
+# MAX_PAGE = [
+#     20,
+#     20
+# ]
 
 
 # スクショ保存時のファイル名を生成
@@ -77,150 +79,196 @@ def main():
             fox = webdriver.Firefox(firefox_profile=profile, firefox_binary=binary,
                                     executable_path='C:\\geckodriver\\geckodriver.exe')
             fox.set_page_load_timeout(6000)
-            try:
-                fox.set_window_size(1280, 720)
+            # try:
+            fox.set_window_size(1280, 720)
 
-                # talent-databank
-                baseUri = baseUris[0]
-                targetUri = targetUris[0]
-                print('\tbaseUri: {} {}'.format(
-                    baseUri, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
-                print('\ttargetUri: {} {}'.format(
-                    targetUri, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
+            # talent-databank
+            baseUri = baseUris[0]
+            targetUri = targetUris[0]
+            print('\tbaseUri: {} {}'.format(
+                baseUri, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
+            print('\ttargetUri: {} {}'.format(
+                targetUri, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
 
-                fox.get(targetUri)
-                time.sleep(1)
-                WebDriverWait(fox, WAITING_TIME).until(EC.presence_of_element_located((By.XPATH, '//body')))
-                print('body', file=logfile, flush=True)
+            # 検索条件
 
-                source = fox.page_source
-                bs = BeautifulSoup(source, 'lxml')
-                print('bs', file=logfile, flush=True)
-                print(source, file=logfile, flush=True)
+            fox.get(targetUri)
+            time.sleep(1)
+            WebDriverWait(fox, WAITING_TIME).until(
+                EC.presence_of_element_located((By.XPATH, '//body')))
+            print('body', file=logfile, flush=True)
 
+            # 「女性」
+            clickSelector(fox, 'input[type="checkbox"][value="female"]')
 
+            #  「タレント・俳優・女優」（最初の要素）の「もっと詳しく」
+            clickLink(fox, 'もっと詳しく')
 
+            # 「女優」
+            clickSelector(fox, 'input[type="checkbox"][value=":女優"]')
 
+            # 年齢
+            clearAndSendKeys(fox, 'age_min', str(AGE_MIN))
+            clearAndSendKeys(fox, 'age_max', str(AGE_MAX))
 
+            # 「すべての条件を併せて検索」
+            clickSelector(
+                fox, 'input[type="image"][src="img/top_search_btn.jpg"]')
 
+            # 検索結果1ページ目
+            time.sleep(1)
+            WebDriverWait(fox, WAITING_TIME).until(
+                EC.presence_of_element_located((By.XPATH, '//body')))
+            print('body', file=logfile, flush=True)
 
+            source = fox.page_source
+            bs = BeautifulSoup(source, 'lxml')
+            print('bs', file=logfile, flush=True)
+            # print(source, file=logfile, flush=True)
 
-
-
+            searchnavi_total = bs.find_all(
+                'span', class_=re.compile('searchnavi_total'))
+            print(searchnavi_total)
+            if len(searchnavi_total) == 0:
                 return
+            count_all = int(searchnavi_total[0].text)
+            last_page = -((-1 * count_all) // PERSONS_PER_PAGE)  # 切り上げ
 
-                # talemecasting-next
-                baseUri = baseUris[1]
-                targetUri = targetUris[1]
-                print('\tbaseUri: {} {}'.format(
-                    baseUri, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
+            for i in range(last_page):
+                print('\tpage: {} {} {}'.format(i, last_page, count_all))
+                print('\tpage: {} {} {} {}'.format(
+                    i, last_page, count_all, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
+                tables = bs.find_all('table', id=re.compile('search-results'))
+                if len(tables) > 0:
+                    table = tables[0]
+                    trs = table.find_all('tr')
+                    print('trs')
+                    print(trs)
+                    if len(trs) > 0:
+                        for tr in trs:
+                            # try:
+                            # 個人
+                            links = tr.find_all('a', class_=re.compile('talent'))
+                            print(links)
+                            if len(links) > 0:
+                                link = links[0]
+                                name = link.text
+                                profile_page = baseUri + link.get('href')
+
+                                # データファイルに出力
+                                print('{}\t\t{}'.format(name, profile_page),
+                                    file=datafile, flush=True)
+
+                            # except expression as identifier:
+                            #     pass
+
+            # TODO:
+            time.sleep(10)
+            return
+
+            # talemecasting-next
+            baseUri = baseUris[1]
+            targetUri = targetUris[1]
+            print('\tbaseUri: {} {}'.format(
+                baseUri, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
+            print('\ttargetUri: {} {}'.format(
+                targetUri, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
+
+            # temp
+
+            # crawler = GoogleImageCrawler(storage={"root_dir": "images"})
+            # crawler.crawl(keyword="犬", max_num=100)
+
+            # サイトごとに一覧を取得
+            for i, targetUri in enumerate(targetUris):
                 print('\ttargetUri: {} {}'.format(
                     targetUri, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
 
+                # category = (targetUri.split(baseUris[i] + '/')[1]).split('/archive/')[0]
 
+                # fox.get(targetUri)
+                # time.sleep(1)
+                # WebDriverWait(fox, WAITING_TIME).until(
+                #     EC.presence_of_element_located((By.CLASS_NAME, 'm-pageNation')))
+                # print('m-pageNation', file=logfile, flush=True)
+                # # time.sleep(1)
 
+                # for i in range(0, MAX_PAGE_PER_CATEGORY):
+                #     print('i: {}'.format(i), file=logfile, flush=True)
 
+                #     # スクレイピング
+                #     if i > 0:
+                #         targetUri = targetUri.split(
+                #             '?bn=')[0] + '?bn='+str(i)+'1'
+                #         print('\ttargetUri: {} {}'.format(
+                #             targetUri, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
+                #         fox.get(targetUri)
+                #         time.sleep(1)
+                #         WebDriverWait(fox, WAITING_TIME).until(
+                #             EC.presence_of_element_located((By.CLASS_NAME, 'm-pageNation')))
+                #         print('m-pageNation', file=logfile, flush=True)
 
+                #     source = fox.page_source
+                #     # BeautifulSoup(source, 'html.parser')
+                #     bs = BeautifulSoup(source, 'lxml')
+                #     print('bs', file=logfile, flush=True)
+                #     # print(source, file=logfile, flush=True)
 
+                #     # 記事毎に取得
+                #     contents = (bs.find_all('div', id='CONTENTS_MAIN'))
+                #     print('i: {} ; contents: {}'.format(i, len(contents)),
+                #           file=logfile, flush=True)
+                #     if len(contents) == 0:
+                #         continue
+                #     cards = contents[0].find_all('div', class_='m-miM09')
+                #     print('i: {} ; count: {}'.format(i, len(cards)),
+                #           file=logfile, flush=True)
 
+                #     for card in cards:
+                #         # 明示的に初期化
+                #         title = ''
+                #         uri = ''
+                #         pubdate = ''
+                #         body = ''
 
+                #         try:
+                #             # print(card.text, file=logfile, flush=True)
+                #             titleclass = (card.find_all('h3', attrs={
+                #                 'class': 'm-miM09_title'}))[0]
+                #             uri = (titleclass.find_all('a'))[0].get("href")
+                #             uri = baseUris[i] + uri
+                #             title = (titleclass.find_all('span', attrs={
+                #                      'class': 'm-miM09_titleL'}))[0].text
+                #             pubdate = (card.find_all('span', attrs={
+                #                        'class': 'm-miM09_date'}))[0].text
+                #             print('\ttitle: ' + title,
+                #                   file=logfile, flush=True)
+                #             print('\turi: ' + uri, file=logfile, flush=True)
+                #             print('\tpubdate: ' + pubdate,
+                #                   file=logfile, flush=True)
 
+                #             # 記事の1ページ目
+                #             print('\tarticleUri: {} {}'.format(
+                #                 uri, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
+                #             fox.get(uri)
 
-                # temp
+                #             # データファイルに出力
+                #             print('{}\t\t{}\t\t{}\t\t{}\t\t{}'.format(
+                #                 category, title, pubdate, uri, body), file=datafile, flush=True)
+                #         except Exception as e:
+                #             print(e, file=logfile, flush=True)
 
-                # crawler = GoogleImageCrawler(storage={"root_dir": "images"})
-                # crawler.crawl(keyword="犬", max_num=100)
-
-                # サイトごとに一覧を取得
-                for i, targetUri in enumerate(targetUris):
-                    print('\ttargetUri: {} {}'.format(
-                        targetUri, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
-
-                    # category = (targetUri.split(baseUris[i] + '/')[1]).split('/archive/')[0]
-
-                    # fox.get(targetUri)
-                    # time.sleep(1)
-                    # WebDriverWait(fox, WAITING_TIME).until(
-                    #     EC.presence_of_element_located((By.CLASS_NAME, 'm-pageNation')))
-                    # print('m-pageNation', file=logfile, flush=True)
-                    # # time.sleep(1)
-
-                    # for i in range(0, MAX_PAGE_PER_CATEGORY):
-                    #     print('i: {}'.format(i), file=logfile, flush=True)
-
-                    #     # スクレイピング
-                    #     if i > 0:
-                    #         targetUri = targetUri.split(
-                    #             '?bn=')[0] + '?bn='+str(i)+'1'
-                    #         print('\ttargetUri: {} {}'.format(
-                    #             targetUri, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
-                    #         fox.get(targetUri)
-                    #         time.sleep(1)
-                    #         WebDriverWait(fox, WAITING_TIME).until(
-                    #             EC.presence_of_element_located((By.CLASS_NAME, 'm-pageNation')))
-                    #         print('m-pageNation', file=logfile, flush=True)
-
-                    #     source = fox.page_source
-                    #     # BeautifulSoup(source, 'html.parser')
-                    #     bs = BeautifulSoup(source, 'lxml')
-                    #     print('bs', file=logfile, flush=True)
-                    #     # print(source, file=logfile, flush=True)
-
-                    #     # 記事毎に取得
-                    #     contents = (bs.find_all('div', id='CONTENTS_MAIN'))
-                    #     print('i: {} ; contents: {}'.format(i, len(contents)),
-                    #           file=logfile, flush=True)
-                    #     if len(contents) == 0:
-                    #         continue
-                    #     cards = contents[0].find_all('div', class_='m-miM09')
-                    #     print('i: {} ; count: {}'.format(i, len(cards)),
-                    #           file=logfile, flush=True)
-
-                    #     for card in cards:
-                    #         # 明示的に初期化
-                    #         title = ''
-                    #         uri = ''
-                    #         pubdate = ''
-                    #         body = ''
-
-                    #         try:
-                    #             # print(card.text, file=logfile, flush=True)
-                    #             titleclass = (card.find_all('h3', attrs={
-                    #                 'class': 'm-miM09_title'}))[0]
-                    #             uri = (titleclass.find_all('a'))[0].get("href")
-                    #             uri = baseUris[i] + uri
-                    #             title = (titleclass.find_all('span', attrs={
-                    #                      'class': 'm-miM09_titleL'}))[0].text
-                    #             pubdate = (card.find_all('span', attrs={
-                    #                        'class': 'm-miM09_date'}))[0].text
-                    #             print('\ttitle: ' + title,
-                    #                   file=logfile, flush=True)
-                    #             print('\turi: ' + uri, file=logfile, flush=True)
-                    #             print('\tpubdate: ' + pubdate,
-                    #                   file=logfile, flush=True)
-
-                    #             # 記事の1ページ目
-                    #             print('\tarticleUri: {} {}'.format(
-                    #                 uri, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
-                    #             fox.get(uri)
-
-                    #             # データファイルに出力
-                    #             print('{}\t\t{}\t\t{}\t\t{}\t\t{}'.format(
-                    #                 category, title, pubdate, uri, body), file=datafile, flush=True)
-                    #         except Exception as e:
-                    #             print(e, file=logfile, flush=True)
-
-            except Exception as e:
-                print(e, file=logfile, flush=True)
-            finally:
-                # 終了時の後片付け
-                try:
-                    fox.close()
-                    fox.quit()
-                    print('Done: {}'.format(
-                        datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
-                except:
-                    pass
+            # except Exception as e:
+            #     print(e, file=logfile, flush=True)
+            # finally:
+            #     # 終了時の後片付け
+            #     try:
+            #         fox.close()
+            #         fox.quit()
+            #         print('Done: {}'.format(
+            #             datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
+            #     except:
+            #         pass
 
 
 def clickClassName(fox, className):
@@ -240,7 +288,7 @@ def clickName(fox, name):
 
 
 def clickSelector(fox, selector):
-    fox.find_elements_by_css_selector(selector).click()
+    fox.find_element_by_css_selector(selector).click()
 
 
 def clickXpath(fox, xpath):
