f893c9c784a8d8322eb2489f0f1403a721e4f11f
YA <YA-androidapp@users.noreply.github.com>
2020-10-05 15:45:23 +0900

    Add face triming

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-# Copyright (c) 2019 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
+# Copyright (c) 2020 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
+
+# pip install icrawler
+# pip install opencv-python opencv-contrib-python
+# pip install selenium
+
 
 from bs4 import BeautifulSoup
 from icrawler.builtin import BingImageCrawler
@@ -10,10 +15,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from seleniumrequests import Firefox
 import codecs
+import cv2
 import datetime
+import numpy as np
 import os
 import re
 import requests
+import tempfile
 import time
 import urllib.parse
 import urllib.request
@@ -25,8 +33,8 @@ print(os.getcwd())
 
 
 # 検索パラメータ
-AGE_MIN = 22
-AGE_MAX = 24
+AGE_MIN = 18
+AGE_MAX = 30
 
 
 # 定数
@@ -35,7 +43,11 @@ DATA_FILEPATH = os.path.join(
 LOG_FILEPATH = os.path.join(
     currentdirectory, 'data', 'log_'+datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.txt')
 
-PHOTO_DIRPATH = os.path.join(currentdirectory, 'img')
+HAARCASCADE_PATH = 'sources/data/haarcascades/haarcascade_frontalface_default.xml'
+faceCascadeClassifier = cv2.CascadeClassifier(HAARCASCADE_PATH)
+
+PHOTO_DIRPATH = os.path.join(currentdirectory, 'data', 'img')
+TEMP_DIRPATH = os.path.join(currentdirectory, 'data', 'temp')
 
 PERSONS_PER_PAGE = 20
 WAITING_TIME = 2000
@@ -68,6 +80,32 @@ def get_filepath():
     return filepath
 
 
+def imread2(filename, flags=cv2.IMREAD_COLOR, dtype=np.uint8):
+    try:
+        n = np.fromfile(filename, dtype)
+        img = cv2.imdecode(n, flags)
+        return img
+    except Exception as e:
+        print(e)
+        return None
+
+
+def imwrite2(filename, img, params=None):
+    try:
+        ext = os.path.splitext(filename)[1]
+        result, n = cv2.imencode(ext, img, params)
+
+        if result:
+            with open(filename, mode='w+b') as f:
+                n.tofile(f)
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(e)
+        return False
+
+
 def collect():
     result_names = []
 
@@ -80,7 +118,7 @@ def collect():
 
     with open(DATA_FILEPATH, 'a', encoding='utf-8') as datafile:
         with open(LOG_FILEPATH, 'a', encoding='utf-8') as logfile:
-            print('Start: {}'.format(
+            print('\tcollect() Start: {}'.format(
                 datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
             binary = FirefoxBinary(
                 'C:\\Program Files\\Mozilla Firefox\\firefox.exe')
@@ -98,9 +136,9 @@ def collect():
                 # talent-databank
                 baseUri = baseUris[0]
                 targetUri = targetUris[0]
-                print('\tbaseUri: {} {}'.format(
+                print('\tcollect() baseUri: {} {}'.format(
                     baseUri, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
-                print('\ttargetUri: {} {}'.format(
+                print('\tcollect() targetUri: {} {}'.format(
                     targetUri, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
 
                 # 検索条件
@@ -109,7 +147,7 @@ def collect():
                 time.sleep(1)
                 WebDriverWait(fox, WAITING_TIME).until(
                     EC.presence_of_element_located((By.XPATH, '//body')))
-                print('body', file=logfile, flush=True)
+                print('\tcollect() body', file=logfile, flush=True)
 
                 # 「女性」
                 clickSelector(fox, 'input[type="checkbox"][value="female"]')
@@ -121,7 +159,8 @@ def collect():
                 # clickSelector(fox, 'input[type="checkbox"][value=":女優"]')
 
                 # 「女優」
-                clickSelector(fox, 'input[type="checkbox"][value="タレント,俳優,女優"]')
+                clickSelector(
+                    fox, 'input[type="checkbox"][value="タレント,俳優,女優"]')
                 clickSelector(fox, 'input[type="checkbox"][value="音楽"]')
                 clickSelector(fox, 'input[type="checkbox"][value="スポーツ"]')
                 clickSelector(fox, 'input[type="checkbox"][value="話す仕事"]')
@@ -139,12 +178,11 @@ def collect():
                 time.sleep(1)
                 WebDriverWait(fox, WAITING_TIME).until(
                     EC.presence_of_element_located((By.XPATH, '//body')))
-                print('body', file=logfile, flush=True)
+                print('\tcollect() body', file=logfile, flush=True)
 
                 source = fox.page_source
                 bs = BeautifulSoup(source, 'lxml')
-                print('bs', file=logfile, flush=True)
-                # print(source, file=logfile, flush=True)
+                print('\tcollect() bs', file=logfile, flush=True)
 
                 searchnavi_total = bs.find_all(
                     'span', class_=re.compile('searchnavi_total'))
@@ -154,7 +192,7 @@ def collect():
                 last_page = -((-1 * count_all) // PERSONS_PER_PAGE)  # 切り上げ
 
                 for i in range(last_page):
-                    print('\tpage: {} {} {} {}'.format(
+                    print('\tcollect() page: {} {} {} {}'.format(
                         i, last_page, count_all, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
                     tables = bs.find_all(
                         'table', id=re.compile('search-results'))
@@ -177,13 +215,14 @@ def collect():
                                         profile_page = baseUri + \
                                             '/search/' + link.get('href')
 
-                                        genre = tr.findAll('td')[3].text.replace('\n', '')
+                                        genre = tr.findAll(
+                                            'td')[3].text.replace('\n', '')
 
                                         result_names.append(name + ' ' + genre)
 
                                         # データファイルに出力
                                         print('{}\t\t{}\t\t{}'.format(name, profile_page, genre),
-                                            file=datafile, flush=True)
+                                              file=datafile, flush=True)
 
                                         try:
                                             imgs = tr.find_all('img')
@@ -202,32 +241,31 @@ def collect():
                         time.sleep(1)
                         WebDriverWait(fox, WAITING_TIME).until(
                             EC.presence_of_element_located((By.XPATH, '//body')))
-                        print('body', file=logfile, flush=True)
+                        print('\tcollect() body', file=logfile, flush=True)
 
                         source = fox.page_source
                         bs = BeautifulSoup(source, 'lxml')
-                        print('bs', file=logfile, flush=True)
+                        print('\tcollect() bs', file=logfile, flush=True)
                     except:
                         break
 
                 # talemecasting-next
                 baseUri = baseUris[1]
                 targetUri = targetUris[1]
-                print('\tbaseUri: {} {}'.format(
+                print('\tcollect() baseUri: {} {}'.format(
                     baseUri, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
-                print('\ttargetUri: {} {}'.format(
+                print('\tcollect() targetUri: {} {}'.format(
                     targetUri, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
 
                 fox.get(targetUri)
                 time.sleep(1)
                 WebDriverWait(fox, WAITING_TIME).until(
                     EC.presence_of_element_located((By.XPATH, '//body')))
-                print('body', file=logfile, flush=True)
+                print('\tcollect() body', file=logfile, flush=True)
 
                 source = fox.page_source
                 bs = BeautifulSoup(source, 'lxml')
-                print('bs', file=logfile, flush=True)
-                # print(source, file=logfile, flush=True)
+                print('\tcollect() bs', file=logfile, flush=True)
 
                 total = bs.find_all(
                     'span', class_=re.compile('required'))
@@ -237,7 +275,7 @@ def collect():
                 last_page = -((-1 * count_all) // PERSONS_PER_PAGE)  # 切り上げ
 
                 for i in range(last_page):
-                    print('\tpage: {} {} {} {}'.format(
+                    print('\tcollect() page: {} {} {} {}'.format(
                         i, last_page, count_all, datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
                     uls = bs.find_all('ul', id=re.compile('display-image'))
                     if len(uls) > 0:
@@ -256,7 +294,7 @@ def collect():
                                         'div', class_='talent-summary')
                                     if len(divs) > 0:
                                         div = divs[0]
-                                        smr  =summaries[0]
+                                        smr = summaries[0]
                                         imgs = div.find_all('img')
                                         if len(imgs) > 0:
                                             img = imgs[0]
@@ -273,11 +311,12 @@ def collect():
 
                                                 genre = smr.find_all(
                                                     'div', class_='genre')[0].text.replace('\n', '')
-                                                result_names.append(name + ' ' + genre)
+                                                result_names.append(
+                                                    name + ' ' + genre)
 
                                                 # データファイルに出力
                                                 print('{}\t\t{}\t\t{}'.format(name, profile_page, genre),
-                                                    file=datafile, flush=True)
+                                                      file=datafile, flush=True)
 
                                 except Exception as e:
                                     print(e, file=logfile, flush=True)
@@ -289,11 +328,11 @@ def collect():
                         time.sleep(1)
                         WebDriverWait(fox, WAITING_TIME).until(
                             EC.presence_of_element_located((By.XPATH, '//body')))
-                        print('body', file=logfile, flush=True)
+                        print('\tcollect() body', file=logfile, flush=True)
 
                         source = fox.page_source
                         bs = BeautifulSoup(source, 'lxml')
-                        print('bs', file=logfile, flush=True)
+                        print('\tcollect() bs', file=logfile, flush=True)
                     except:
                         break
 
@@ -304,10 +343,10 @@ def collect():
                 try:
                     fox.close()
                     fox.quit()
-                    print('Done: {}'.format(
+                    print('\tcollect() Done: {}'.format(
                         datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')), file=logfile, flush=True)
                 except:
-                    pass
+                    print(e, flush=True)
     return result_names
 
 
@@ -357,12 +396,51 @@ def getFilename(url):
 
 
 def download_img(url, filename_prefix):
-    r = requests.get(url, stream=True)
-    if r.status_code == 200:
-        with open(os.path.join(PHOTO_DIRPATH, filename_prefix + '_' + getFilename(url)), 'wb') as f:
-            f.write(r.content)
+    with open(LOG_FILEPATH, 'a', encoding='utf-8') as logfile:
+        print('\tdownload_img() LOG: {} {} {}'.format(
+            datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'), url, filename_prefix), file=logfile, flush=True)
+        r = requests.get(url, stream=True)
+        if r.status_code == 200:
+            print('\tdownload_img() get: {} {} {} 200'.format(
+                datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'), url, filename_prefix), file=logfile, flush=True)
+            with open(os.path.join(PHOTO_DIRPATH, filename_prefix + '_' + getFilename(url)), 'wb') as f:
+                f.write(r.content)
+                print('\tdownload_img() write: {} {} {} 200 f'.format(
+                    datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'), url, filename_prefix), file=logfile, flush=True)
+
+                img = imread2(f.name)
+                print('\tdownload_img() imread2: {} {} {} img f type:{}'.format(
+                    datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'), url, filename_prefix, type(img)), file=logfile, flush=True)
+
+                if img is not None:
+                    print('\tdownload_img() img is not Null: {} {} {}'.format(
+                        datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'), url, filename_prefix), file=logfile, flush=True)
+                    src_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                    faces = faceCascadeClassifier.detectMultiScale(src_gray)
+                    print('\tdownload_img() faces: {} {} {}'.format(
+                        datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'), url, filename_prefix), file=logfile, flush=True)
+                    for x, y, w, h in faces:
+                        print('\tdownload_img() faces: {} {} {} xywh: {} {} {} {}'.format(
+                            datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'), url, filename_prefix, x, y, w, h), file=logfile, flush=True)
+                        try:
+                            face = img[y: y + h, x: x + w]
+                            filesplt = os.path.splitext(
+                                os.path.basename(getFilename(url)))
+                            facefile = os.path.join(
+                                PHOTO_DIRPATH, filename_prefix + '_' + filesplt[0] + '_{:04}-{:04}-{:04}-{:04}'.format(y, y + h, x, x + w) + filesplt[1])
+                            imwrite2(facefile, face)
+                        except Exception as e:
+                            print('Exception: {} {}'.format(
+                                datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'), e), file=logfile, flush=True)
+        else:
+            print('\tdownload_img() get: {} {} {} status: {}'.format(
+                datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'), url, filename_prefix, r.status_code), file=logfile, flush=True)
 
 
 if __name__ == '__main__':
+    with open(LOG_FILEPATH, 'a', encoding='utf-8') as logfile:
+        print('Start', file=logfile, flush=True)
     names = collect()
     search(names)
+    with open(LOG_FILEPATH, 'a', encoding='utf-8') as logfile:
+        print('Done', file=logfile, flush=True)
